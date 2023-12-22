bde13b9101fb56756c3d7aef46ebe0ca7cdb87da
why-not-available <131629649+why-not-available@users.noreply.github.com>
2023-12-22 18:38:14 +0900

    [level unrated] Title: 부분 문자열인지 확인하기, Time: 0.02 ms, Memory: 76.1 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181843. 부분 문자열인지 확인하기/부분 문자열인지 확인하기.java
@@ -0,0 +1,10 @@
+// contains()
+class Solution {
+    public int solution(String my_string, String target) {
+        
+        if(my_string.contains(target)){
+            return 1;
+        }   return 0;
+        
+    }
+}
