77e150d9670c6e6923feb89da2e8901a4d9ba659
why-not-available <131629649+why-not-available@users.noreply.github.com>
2023-12-24 23:26:27 +0900

    [level unrated] Title: 공백으로 구분하기 2, Time: 0.28 ms, Memory: 72.3 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181868. 공백으로 구분하기 2/공백으로 구분하기 2.java
@@ -0,0 +1,8 @@
+class Solution {
+    public String[] solution(String my_string) {
+        // String[] answer = {};
+        
+        return my_string.trim().split("\\s+");
+        // return answer;
+    }
+}
