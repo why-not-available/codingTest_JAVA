3a17be257d0ed6ad95d498cde4106ee9dbfd6203
why-not-available <131629649+why-not-available@users.noreply.github.com>
2023-12-29 20:24:37 +0900

    [level unrated] Title: 제곱수 판별하기, Time: 0.03 ms, Memory: 74.6 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120909. 제곱수 판별하기/제곱수 판별하기.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        
+        for(int i=1; i<=n; i++){
+            if(i*i==n){
+                return 1;
+            }else{
+                answer = 2;
+            }
+        }
+        
+        return answer;
+    }
+}
