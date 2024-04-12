1f80e73df052d6864be91f0c6a91b40ba85cf559
why-not-available <131629649+why-not-available@users.noreply.github.com>
2024-04-12 15:00:25 +0900

    [level 0] Title: 9로 나눈 나머지, Time: 0.04 ms, Memory: 77.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181914. 9로 나눈 나머지/9로 나눈 나머지.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int solution(String number) {
+        int answer = 0;
+        
+        for(int i = 0; i<number.length(); i++){
+            answer += Integer.valueOf(number.substring(i, i+1));
+        }
+        
+        return answer%9;
+    }
+}
