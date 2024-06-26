2739f860b6f11214444767513da8363e638cb0cf
why-not-available <131629649+why-not-available@users.noreply.github.com>
2024-06-26 22:23:51 +0900

    [level 0] Title: 369게임, Time: 7.28 ms, Memory: 73.4 MB -BaekjoonHub

--- 프로그래머스/0/120891. 369게임/369게임.java
+++ 프로그래머스/0/120891. 369게임/369게임.java
@@ -1,7 +1,8 @@
 class Solution {
     public int solution(int order) {
         int answer = 0;
-        String orderArr[] = Integer.toString(order).split("");
+        // String orderArr[] = Integer.toString(order).split("");
+        String orderArr[] = (order+"").split("");
         
         for(int i=0; i<orderArr.length; i++){
             if(orderArr[i].equals("3") || orderArr[i].equals("6") || orderArr[i].equals("9")){
