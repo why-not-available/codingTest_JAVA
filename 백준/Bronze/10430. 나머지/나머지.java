3115ff4ebcbc3a901f9d568ed116fda1e5c6463a
why-not-available <131629649+why-not-available@users.noreply.github.com>
2025-02-07 21:34:06 +0900

    [Bronze V] Title: 나머지, Time: 96 ms, Memory: 12848 KB -BaekjoonHub

---
+++ 백준/Bronze/10430. 나머지/나머지.java
@@ -0,0 +1,20 @@
+//(A+B)%C
+//((A%C) + (B%C))%C 
+//(A×B)%C 
+//((A%C) × (B%C))%C
+
+import java.util.*;
+
+public class Main{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int A=sc.nextInt();
+        int B=sc.nextInt();
+        int C=sc.nextInt();
+        
+        System.out.println((A+B) % C);
+        System.out.println(((A%C) + (B%C)) % C);
+        System.out.println((A*B) % C);
+        System.out.print(((A%C)*(B%C)) % C);
+    }
+}
