c10a6d54f9e6e00e3a13f2e9cbf121e66f717e32
why-not-available <131629649+why-not-available@users.noreply.github.com>
2025-02-09 17:31:34 +0900

    [Bronze V] Title: 꼬마 정민, Time: 92 ms, Memory: 12840 KB -BaekjoonHub

---
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class Main{
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+        long a = scan.nextLong();
+        long b = scan.nextLong();
+        long c = scan.nextLong();
+        
+        System.out.print(a+b+c);
+    }
+}
