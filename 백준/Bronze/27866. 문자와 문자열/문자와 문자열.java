8883dbd69dbec7a6d356cb9f512db2d41f6ebc7a
why-not-available <131629649+why-not-available@users.noreply.github.com>
2025-02-18 22:53:21 +0900

    [Bronze V] Title: 문자와 문자열, Time: 92 ms, Memory: 12872 KB -BaekjoonHub

---
+++ 백준/Bronze/27866. 문자와 문자열/문자와 문자열.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class Main{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine(); //: 단어
+        int i = sc.nextInt()-1; //: i번째
+        
+        System.out.print(s.charAt(i));
+        
+        
+    }
+}
