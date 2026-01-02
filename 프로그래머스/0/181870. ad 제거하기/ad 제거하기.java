4e447546eeebdf666cd99f522bb213afe51dc111
why-not-available <131629649+why-not-available@users.noreply.github.com>
2026-01-02 18:09:09 +0900

    [level 0] Title: ad 제거하기, Time: 0.77 ms, Memory: 84.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181870. ad 제거하기/ad 제거하기.java
@@ -0,0 +1,18 @@
+// contains, indexOf
+import java.util.ArrayList;
+import java.util.List;
+
+class Solution {
+    public String[] solution(String[] strArr) {
+        // String[] answer = {};
+        List<String> answer = new ArrayList<>();
+
+        for(int i=0; i<strArr.length; i++){
+            if(!strArr[i].contains("ad")){
+                answer.add(strArr[i]);            
+            }
+        }
+        
+        return answer.toArray(new String[0]);
+    }
+}
